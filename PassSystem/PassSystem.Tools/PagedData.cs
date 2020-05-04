ad52aa609f3663f874741cd1b0c42c5467431e43
a.lifantev <a.lifantev@virtuumlab.ru>
2020-05-04 15:42:48 +0300

    added tools project

---
+++ PassSystem/PassSystem.Tools/PagedData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PassSystem.Tools
+{
+    public class PagedData<T> where T: class
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int TotalRows { get; set; }
+    }
+}
---
+++ PassSystem/PassSystem.Tools/Result.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PassSystem.Tools
+{
+    public class Result<T>
+    {
+        private List<String> _errors;
+        public T Data { get; set; }
+        public List<String> Errors { get; set; }
+
+        public Boolean IsSuccess => !Errors?.Any() ?? true;
+        public Boolean IsFailed => Errors?.Any() ?? false;
+
+        public static Result<T> Success(T data)
+        {
+            return new Result<T>()
+            {
+                Data = data
+            };
+        }
+        
+        public static Result<T> Failed(String error)
+        {
+            return new Result<T>()
+            {
+                Errors = new List<string>(){error}
+            };
+        }
+        
+        public static Result<T> Failed(IEnumerable<String> errors)
+        {
+            return new Result<T>()
+            {
+                Errors = errors.ToList()
+            };
+        }
+        
+        
+
+    }
+}
